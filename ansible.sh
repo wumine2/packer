63ccb8d2b7e383dd045f9ca19e3376b7cab92bc5
Wunmi <atiwunmi@yahoo.com>
2023-10-05 09:21:33 -0600

    updated

---
+++ ansible.sh
@@ -0,0 +1,7 @@
+# ansible.sh
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update
+sudo apt-get install ansible -y
